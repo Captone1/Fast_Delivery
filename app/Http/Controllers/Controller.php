10d7deb589a16b50e2fe94b69bf8a466e98295ba
lê như hiếu <lenhuhieu121@gmail.com>
2020-10-06 07:44:35 +0700

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function uploadFile($file){
+
+        $fileName = time().$file->getClientOriginalName();
+        $destinationPath = public_path('images');
+        if ($file->isValid()) {
+
+            $file->move($destinationPath, $fileName);
+            return $fileName;
+        }
+        return null;
+    }
+}
