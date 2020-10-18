ea9e26f6fddd92e1f4b674673d44121efbf994ac
lê như hiếu <lenhuhieu121@gmail.com>
2020-10-18 21:13:09 +0700

    forgot

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -28,25 +28,5 @@ class Controller extends BaseController
         $fileLocation = $destinationPath.'/'.$fileName;
         unlink($fileLocation);
     }
-    public function getBill($params){
-
-        $data = Bill::orderBy('id','desc');
-        if (isset($params['status'])){
-            $data = $data->where('status',$params['status']);
-        }
-        if (isset($params['staff_id'])){
-            $data = $data->where('staff_id',$params['staff_id']);
-        }
-        if (isset($params['date'])){
-            $data = $data->whereBetween('created_at',$params['date']);
-        }
-        if (isset($params['transport_id'])){
-            $data = $data->where('transport_id',auth()->user()->transport_id);
-        }
-//        if (auth()->user()->role == 'staff'){
-//            $data = $data->where('staff_id',auth()->id());
-//        }
-        $data = $data->get();
-        return $data;
-    }
+//    
 }
